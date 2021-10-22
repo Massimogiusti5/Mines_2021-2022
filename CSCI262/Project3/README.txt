9e261c13c8364cc1537e746236e4a9d48a7a684a
Massimogiusti5 <massimogiusti@live.com>
2021-10-22 17:37:02 -0600

    Added Project 3

---
+++ CSCI262/Project3/README.txt
@@ -0,0 +1,24 @@
+Massimo Giusti
+Project 3: Evil Hangman
+10/22/2021
+
+1) collaborators: none
+
+2) A challenge I had with this project was figuring out where to start. 
+   When initally looking at the source code as well as the course page,
+   I was very confused as to what the source code wanted me to do. At first
+   I was thinking about this problem top-down, but by changing my design 
+   process to bottom up made me better understand the source code as well
+   as the workings of the eventual game. For example, by initially coding up 
+   the prompts and all the helper functions, I eventually got to solving the 
+   problem one step at a time and it was straightforward from there.
+
+3) What I liked about this assignment was the use of multiple data structures 
+   for optimized execution. For example, I had initially drafted my solution to use
+   vectors, but I saw that vectors were ineffective due to the amount of insertion/deletion 
+   and comparisons the game had to do. I then integrated a mix of vectors, sets and maps to
+   improve my solution, and that was the fun part of the project. 
+
+4) I spent about 4 and a half hours on this assignment.
+
+5) no EC features
---
+++ CSCI262/Project3/hangman.h
@@ -0,0 +1,73 @@
+#ifndef _HANGMAN_H
+#define _HANGMAN_H
+
+/*
+    hangman.h
+        
+    Class definition for the hangman class.
+
+    assignment: CSCI 262 Project - Evil Hangman        
+
+    author: 
+
+    last modified: 9/24/2017
+*/
+
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+/******************************************************************************
+   class hangman
+
+   Maintains game state for a game of hangman.
+
+******************************************************************************/
+
+class hangman {
+public:
+    hangman();
+
+    // start a new game where player gets num_guesses unsuccessful tries
+	void start_new_game(int num_guesses, int length);
+
+    // player guesses letter c; return whether or not char is in word
+    bool process_guess(char c);
+
+    // display current state of word - guessed characters or '-'
+    string get_display_word();
+
+    // How many guesses remain?
+	int get_guesses_remaining();
+
+    // What characters have already been guessed (for display)?
+    string get_guessed_chars();
+
+    // Has this character already been guessed?
+    bool was_char_guessed(char c);
+
+    string print_remaining();
+
+    // Has the game been won/lost?  (Else, it continues.)
+    bool is_won();
+    bool is_lost();
+
+    // Return the true hidden word.
+    string get_hidden_word();
+
+private:
+    unordered_set<string> words;
+    unordered_map<string,vector<string>> families;
+    int guesses;
+    bool showRemaining;
+    string guessedChars;
+    string family;
+    string hiddenWord;
+    bool isWon;
+    bool isLost;
+};
+
+#endif
